8b0f04f42186056f209f32e58271bfb43a144aa9
S M Areeb <smareeb@ymail.com>
2023-06-19 17:47:49 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import "preact/debug";
 import { render } from "preact";
 import html from "html";
-import App from "/src/app.js";
+import App from "./src/App.js";
 
 render(html`<${App} />`, document.getElementById("root"));
