ef3ca9fd1af144b7801c6ede0f6161baf77c693e
sm-areeb <smareeb@ymail.com>
2024-02-03 19:58:04 +0530

    styled the header

--- src/RootLayout.js
+++ src/RootLayout.js
@@ -2,16 +2,29 @@ import { Outlet, Link } from "react-router-dom";
 export default RootLayout = () => {
   return (
     <>
-      <header>
-        header <Link to="">Browse Packages</Link> |{" "}
-        <Link to="/create">Create a Package</Link>
+      <header className="py-3">
+        <nav className="level">
+          <p className="level-item has-text-centered">
+            <h3 className="title">Book a Trip</h3>
+          </p>
+          <p className="level-item has-text-centered">
+            <Link to="" className="link is-info">
+              Browse Packages
+            </Link>{" "}
+          </p>
+          <p className="level-item has-text-centered">
+            <Link to="/create" className="link is-info">
+              Create a Package
+            </Link>
+          </p>
+        </nav>
       </header>
       <main className="container is-fluid">
         <div className="container">
           <Outlet />
         </div>
       </main>
-      <footer>footer</footer>
+      <footer></footer>
     </>
   );
 };
