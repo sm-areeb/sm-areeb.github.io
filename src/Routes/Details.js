138420037e6860caf95a6b4e13fa05565b8e151c
sm-areeb <smareeb@ymail.com>
2024-02-04 21:17:55 +0530

    added details on details page

--- src/Routes/Details.js
+++ src/Routes/Details.js
@@ -1,20 +1,64 @@
 import { useLocation } from "react-router-dom";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import ItineraryPanel from "../components/ItineraryPanel";
 
 export default Details = () => {
   const location = useLocation();
   const details = location.state;
   return (
     <div className="my-5 browse">
-      <h1 className="title mb-5 has-text-centered">{details.name}</h1>
-      <Carousel className="details-carousel">
-        {details.photos.map((i) => (
-          <figure className="image is-4by3">
-            <img src={i} alt="Placeholder image" />
-          </figure>
-        ))}
-      </Carousel>
+      <h1 className="title has-text-centered">{details.name}</h1>
+      <div className="narrow-container">
+        <article className="message is-dark my-5">
+          <h4 className="subtitle has-text-weight-bold">
+            Destinations - {details.destinations.join(", ")}
+          </h4>
+          <div className="message-body">{details.description}</div>
+        </article>
+        <Carousel>
+          {details.photos.map((i) => (
+            <figure className="image is-4by3">
+              <img src={i} alt="Placeholder image" />
+            </figure>
+          ))}
+        </Carousel>
+        <ItineraryPanel itinerary={details.itinerary} />
+
+        <nav className="level my-5">
+          <div className="level-item has-text-centered">
+            <div>
+              <p className="heading">Duration</p>
+              <p className="title">
+                {details.duration} days / {details.duration - 1} nights
+              </p>
+            </div>
+          </div>
+          <div className="level-item has-text-centered">
+            <div>
+              <p className="heading">Price</p>
+              <p className="title">${details.discountedPrice}</p>
+            </div>
+          </div>
+        </nav>
+        <nav className="level my-5 py-5">
+          <div className="level-item has-text-centered">
+            <div class="select is-rounded">
+              <select>
+                <option>Select a Date</option>
+                {details.bookings.map(({ available, date }) => (
+                  <option key={date} disabled={!available}>
+                    {date}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+          <div className="level-item has-text-centered">
+            <button class="button is-success is-rounded">Book now</button>
+          </div>
+        </nav>
+      </div>
     </div>
   );
 };
---
+++ src/components/ItineraryPanel.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export default ItineraryPanel = ({ itinerary }) => {
+  const [activeTab, setActiveTab] = useState(0);
+  return (
+    <div className="mt-5">
+      <article className="panel is-danger">
+        <p className="panel-heading">Itinerary</p>
+        <p className="panel-tabs">
+          {itinerary.map((_, index) => (
+            <a
+              key={index}
+              className={(activeTab == index && "is-active") || ""}
+              onClick={() => setActiveTab(index)}
+            >
+              Day {index + 1}
+            </a>
+          ))}
+        </p>
+        {itinerary[activeTab].map((day) => {
+          return (
+            <a className="panel-block has-text-weight-semibold">
+              <span className="panel-icon">
+                <i>&#x1F550;</i>
+              </span>
+              {day.time} - {day.activity}
+            </a>
+          );
+        })}
+      </article>
+    </div>
+  );
+};
